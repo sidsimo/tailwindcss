c80bd1dee8318c0de3c6c428a4cb815d12a9f636
Jems Divin <yassindivin200@gmail.com>
2024-11-01 23:01:10 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const cards3 = document.getElementById("cards3");
 const cards4 = document.getElementById("cards4");
 const btn = document.getElementById("btn");
+const points = document.getElementById("points");
+const x = document.getElementById("x");
+const hidshow = document.getElementById("hidshow");
 btn.addEventListener("click" , function () {
 });
+points.onclick = function() {
+    hidshow.style.right = "0.8rem";
+}
+x.onclick = function() {
+    hidshow.style.right = "-100%";
+}
+window.alert("Welcome you in my web");
