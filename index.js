8c1b249b6a70289e2a4c11845e094b91d1793f60
Jems Divin <yassindivin200@gmail.com>
2024-11-02 15:39:53 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+const cards1 = document.getElementById("cards1");
+const cards2 = document.getElementById("cards2");
 const cards3 = document.getElementById("cards3");
 const cards4 = document.getElementById("cards4");
 const btn = document.getElementById("btn");
+const btnx = document.getElementById("btnx");
 const points = document.getElementById("points");
 const x = document.getElementById("x");
 const hidshow = document.getElementById("hidshow");
@@ -12,6 +15,19 @@ points.onclick = function() {
 x.onclick = function() {
     hidshow.style.display = "none";
 }
-document.body.scroll({
+window.scroll({
     behavior : "smooth"
 });
+
+btn.onclick = function () {
+    cards3.style.display = "flex";
+    cards4.style.display = "flex";
+    btn.style.display = "none";
+    btnx.style.display = "block";
+}
+btnx.onclick = function () {
+    cards3.style.display = "none";
+    cards4.style.display = "none";
+    btnx.style.display = "none";
+    btn.style.display = "block";
+}
